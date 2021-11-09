3d373102f36c5c27542c74aae214a2232e65cf04
EmsCode82 <emsgithub@gmail.com>
2021-11-09 15:09:55 -0600

    .

---
+++ src/package1/Hello.java
@@ -0,0 +1,21 @@
+package package1;
+public class Hello {
+
+	public static void main(String[] args) {
+		//boolean x = !(4>5);//
+		//System.out.println(x);//
+		/*boolean x;
+		
+		x = (5<4)?true:false;
+		
+		System.out.println(x);
+		*/
+		int x = 10;
+		
+		x = (10 == x) ? 1 : 0;
+		System.out.println(x);
+		
+		
+	}
+
+}
